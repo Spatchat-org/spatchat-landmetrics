19955342bac57b282aea4045aa71ee9e09a8dc50
Ho Yi Wan <hoyiwan@users.noreply.huggingface.co>
2025-07-23 18:00:22 +0000

    Update app.py

--- app.py
+++ app.py
@@ -359,7 +359,7 @@ with gr.Blocks(title="Spatchat") as iface:
              show_share_button=False, elem_id="logo-img")
     gr.HTML("<style>#logo-img img{height:90px;margin:10px;border-radius:6px;}</style>")
     gr.Markdown("## 🌲 Spatchat: Landscape Metrics Assistant {landmetrics}")
-    gr.HTML(\"\"\"
+    gr.HTML("""
       <div style="margin-top: -10px; margin-bottom: 15px;">
         <input type="text" value="https://spatchat.org/browse/?room=landmetrics" id="shareLink" readonly style="width: 50%; padding: 5px; background-color: #f8f8f8; color: #222; font-weight: 500; border: 1px solid #ccc; border-radius: 4px;">
         <button onclick="navigator.clipboard.writeText(document.getElementById('shareLink').value)" style="padding: 5px 10px; background-color: #007BFF; color: white; border: none; border-radius: 4px; cursor: pointer;">
@@ -371,14 +371,14 @@ with gr.Blocks(title="Spatchat") as iface:
           <a href="https://www.facebook.com/sharer/sharer.php?u=https://spatchat.org/browse/?room=landmetrics" target="_blank">📘 Facebook</a>
         </div>
       </div>
-    \"\"\")
-    gr.HTML(\"\"\"
+    """)
+    gr.HTML("""
       <div style="font-size: 14px;">
       © 2025 Ho Yi Wan & Logan Hysen. All rights reserved.<br>
       If you use Spatchat in research, please cite:<br>
       <b>Wan, H.Y.</b> & <b>Hysen, L.</b> (2025). <i>Spatchat: Landscape Metrics.</i>
       </div>
-    \"\"\")
+    """)
 
     with gr.Row():
         with gr.Column(scale=1):
